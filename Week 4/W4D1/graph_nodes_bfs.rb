cff9bac925397b3aaccc49eae4fa90cb5d0767d1
Mike <letumichael@gmail.com>
2021-07-26 12:20:28 -0700

    Finished Graph Node Project - implemented with a set and BFS

--- Week 4/W4D1/graph_nodes_bfs.rb
+++ Week 4/W4D1/graph_nodes_bfs.rb
@@ -7,4 +7,24 @@ class GraphNode
         @value = value
         @neighbors = []
     end
+
+    def add_neighbors(node)
+        neighbors << node
+    end
+
+    def bfs(start_node, target)
+        queue = [start_node]
+        visited_nodes = Set.new
+
+        until queue.empty?
+            node = queue.shift
+            unless visited_nodes.include?(node)
+                return node.value if node.value == target
+                visited_nodes.add(node)
+                queue += node.neighbors
+            end
+        end
+
+        nil
+    end
 end
