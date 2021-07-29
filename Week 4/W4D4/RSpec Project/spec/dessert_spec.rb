1c70b561ffabe269ce545db0da2266bd1c961d67
Mike <letumichael@gmail.com>
2021-07-28 21:41:27 -0700

    Finished writing unit tests for each method for Dessert

--- Week 4/W4D4/RSpec Project/spec/dessert_spec.rb
+++ Week 4/W4D4/RSpec Project/spec/dessert_spec.rb
@@ -24,24 +24,47 @@ describe Dessert do
   end
 
   describe "#add_ingredient" do
-    it "adds an ingredient to the ingredients array"
+    it "adds an ingredient to the ingredients array" do
+      waffle.add_ingredient("chocolate chips")
+      expect(waffle.ingredients).to include("chocolate chips")
+    end
   end
 
   describe "#mix!" do
-    it "shuffles the ingredient array"
+    it "shuffles the ingredient array" do
+      ingredients = ["flour", "eggs", "butter", "chocolate chips"]
+      ingredients.each { |ingredient| waffle.add_ingredient(ingredient) }
+
+      expect(waffle.ingredients).to eq(ingredients)
+      waffle.mix!
+      expect(waffle.ingredients).to_not eq(ingredients)
+      expect(waffle.ingredients.sort).to eq(ingredients.sort)
+    end
   end
 
   describe "#eat" do
-    it "subtracts an amount from the quantity"
+    it "subtracts an amount from the quantity" do
+      current_quantity = waffle.quantity
+      waffle.eat(10)
+      expect(waffle.quantity).to eq(90)
+    end
 
-    it "raises an error if the amount is greater than the quantity"
+    it "raises an error if the amount is greater than the quantity" do
+      expect { waffle.eat(200) }.to raise_error("not enough left!")
+    end
   end
 
   describe "#serve" do
-    it "contains the titleized version of the chef's name"
+    it "contains the titleized version of the chef's name" do
+      allow(chef).to receive(:titleize).and_return("Chef Mike the Terrible Baker")
+      expect(waffle.serve).to eq("Chef Mike the Terrible Baker has made 100 waffles!")
+    end
   end
 
   describe "#make_more" do
-    it "calls bake on the dessert's chef with the dessert passed in"
+    it "calls bake on the dessert's chef with the dessert passed in" do
+      expect(chef).to receive(:bake).with(waffle)
+      waffle.make_more
+    end
   end
 end
