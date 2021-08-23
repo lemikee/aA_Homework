438b80506575f05faf4ff7196830f7d8245be11d
Mike Le <letumichael@gmail.com>
2021-08-23 16:38:24 -0700

    Completed fizzBuzz problem

--- Week 8/W8D2/Intro Javascript Problems (Part 1)/problems.js
+++ Week 8/W8D2/Intro Javascript Problems (Part 1)/problems.js
@@ -9,3 +9,17 @@ function madLib(verb, adj, noun) {
 function isSubstring(phrase, subphrase) {
   return phrase.includes(subphrase);
 }
+
+// fizzBuzz
+
+function fizzBuzz(array) {
+  const fizzBuzzArr = [];
+
+  array.forEach(el => {
+    if ((el % 3 === 0) ^ (el % 5 === 0)) {
+      fizzBuzzArr.push(el);
+    }
+  });
+
+  return fizzBuzzArr;
+}
