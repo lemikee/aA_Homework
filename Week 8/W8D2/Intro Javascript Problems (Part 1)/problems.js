55f3773e9832ee62c1324f7b2c37aedd3d982d9a
Mike Le <letumichael@gmail.com>
2021-08-23 16:35:05 -0700

    Complete isSubstring problem

--- Week 8/W8D2/Intro Javascript Problems (Part 1)/problems.js
+++ Week 8/W8D2/Intro Javascript Problems (Part 1)/problems.js
@@ -3,3 +3,9 @@
 function madLib(verb, adj, noun) {
   return `We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`
 }
+
+// isSubstring
+
+function isSubstring(phrase, subphrase) {
+  return phrase.includes(subphrase);
+}
